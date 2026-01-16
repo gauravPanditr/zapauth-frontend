b2220838bdceddc10a0a9fb404eca211284669fe
gauravPanditr <gauravpandit7856@gmail.com>
2026-01-16 18:03:38 +0530

    install tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { ArrowUpIcon } from "lucide-react"
-import { Button } from "@/components/ui/button"
+
 function App() {
  
 
   return (
     <>
-     <div className="flex flex-wrap items-center gap-2 md:flex-row">
-      <Button variant="outline">Button</Button>
-      <Button variant="outline" size="icon" aria-label="Submit">
-        <ArrowUpIcon />
-      </Button>
-    </div>
+     
+    <h1 className="">
+      
+    </h1>
     </>
   )
 }
